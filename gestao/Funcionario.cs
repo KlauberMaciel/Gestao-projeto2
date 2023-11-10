c2653c5c8cd9ea3845ed788d7e31943cf1cf4ea8
Klauber Maciel <klaubermrocha@gmail.com>
2023-11-10 19:29:10 -0300

    criaçao de objetos na models

---
+++ gestao/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    internal class Funcionario
+    {
+        public int Id { get; set; }
+        public string NomeUsuario { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public DateTime Data_Nasc { get; set; }
+        public string Celular { get; set; }
+        public string Senha { get; set; }
+        public string Endereco { get; set; }
+        public List<Cargo> Cargo { get; set; }
+    }
+}
---
+++ gestao/Pedido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public float Valor_total { get; set; }
+        public DateTime Data_pedido { get; set; }
+        public List<Cliente> Clientes { get; set; }
+        public List<Funcionario> Funcionarios { get; set; }
+        public Pedido() 
+        { 
+           Clientes = new List<Cliente>();
+           Funcionarios = new List<Funcionario>();
+        } 
+    }
+}
+
+
